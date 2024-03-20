a79afa562336db1099cda48bc03d90d8cab58387
Claudia M Cardona H <80611384+claumcarh@users.noreply.github.com>
2024-03-19 19:57:16 -0500

    se define propiedad de encriptar y desencriptar

--- src/components/textArea.jsx
+++ src/components/textArea.jsx
@@ -1,17 +1,64 @@
+import { useState } from 'react';
+import Button from './button';
+import '@/assets/styles/components/textArea.scss';
 
-function TextArea() {
+
+
+function TextArea({ onEncryptedText, onDencryptedText }) {
+  const [text, setText] = useState('');
+
+  function encryptText() {
+    const encryptedText = text
+      .replaceAll(/e/gi, 'enter')
+      .replaceAll(/i/gi, 'imes')
+      .replaceAll(/a/gi, 'ai')
+      .replaceAll(/o/gi, 'ober')
+      .replaceAll(/u/gi, 'ufat');
+
+    onEncryptedText(encryptedText);
+  }
+
+  function decryptText() {
+    const decryptedText = text
+      .replaceAll(/enter/gi, 'e')
+      .replaceAll(/imes/gi, 'i')
+      .replaceAll(/ai/gi, 'a')
+      .replaceAll(/ober/gi, 'o')
+      .replaceAll(/ufat/gi, 'u');
+
+    onDencryptedText(decryptedText);
+  }
+
+  function handleChangeText(e) {
+    setText(e.target.value);
+  }
+
   return (
-    <>
-       <h1 className="principalText">
-        Ingresa el texto aqui
-      </h1>
+    <div>
+      <textarea
+        className="textArea__text"
+        placeholder="Ingresa el texto aqui"
+        value={text}
+        onChange={handleChangeText}
+      />
 
-      <p>Solo letras minúsculas y sin acentos</p>
-      <button className="botonEncriptar">Encriptar</button>
-      <button className="botonDesencriptar">Desencriptar</button>
-    </>
+      <p className="textArea__note">Solo letras minúsculas y sin acentos</p>
+      <div className="textArea__button-container">
+        <Button
+          modifier="primary"
+          onClick={() => encryptText()}
+        >
+          Encriptar
+        </Button>
+        <Button
+          modifier="secondary"
+          onClick={() => decryptText()}
+        >
+          Desencriptar
+        </Button>
+      </div>
+    </div>
   );
 }
 
 export default TextArea;
-
